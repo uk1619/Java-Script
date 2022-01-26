ac7424f5c688a60357685ab60d85045ce7b01424
uk1619 <98322079+uk1619@users.noreply.github.com>
2022-01-26 21:14:49 +0530

    DAY2 assignment

---
+++ DAY2 ASSIGNMENT/bool.js
@@ -0,0 +1,15 @@
+const myvar1 = 5 > 10; //false
+
+const myvar2 = 5 < 10; //true
+
+const myvar3 = 5==5;  //true
+
+const myvar4 = 5==10;  //false
+
+console.log(myvar1);
+
+console.log(myvar2);
+
+console.log(myvar3);
+
+console.log(myvar4);
